3c5f721f72d384a066ea2e810bdf7a4d11643e54
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-03-08 14:11:00 +0900

    Title: 길이 단위 변환하기, Time: 165ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240308/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,7 @@
+let ft = 30.48;
+let mi = 160934;
+
+let num1 = 9.2;
+let num2 = 1.3;
+console.log('%dft = %dcm', num1, (num1*ft).toFixed(1));
+console.log('%dmi = %dcm', num2, (num2*mi).toFixed(1));
