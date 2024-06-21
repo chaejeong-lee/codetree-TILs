74aa4b31ecfdec7057bdbae596c01bb2f5798a2c
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-06-21 23:45:15 +0900

    Title: 블럭의 총 둘레, Time: 135ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240621/블럭의 총 둘레/total-perimeter-of-blocks.java
+++ 240621/블럭의 총 둘레/total-perimeter-of-blocks.java
@@ -2,24 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static int N;
+    private static int[][] map;
+    private static int answer = 0;
+
+    private static final int MAX_SIZE = 101;
+    
+    private static int[] dr = {-1, 0, 1, 0};
+    private static int[] dc = {0, 1, 0, -1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        map = new int[MAX_SIZE][MAX_SIZE];
 
-        int minX = 100, minY = 100;
-        int maxX = 0, maxY = 0;
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine()," ");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            minX = minX>x?x:minX;
-            minY = minY>y?y:minY;
-            maxX = maxX<x?x:maxX;
-            maxY = maxY<y?y:maxY;
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            map[r-1][c-1] = 1;
         }
-        System.out.println(((maxX - minX + 1) + (maxY - minY + 1))*2);
+        bfs(0, 0);
+        System.out.println(answer);
+    }
 
+    private static void bfs(int r, int c) {
+        if(map[r][c] == 1) {
+            answer++;
+            return;
+        }
+        map[r][c] = 2;
+
+        for(int d=0;d<4;d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+
+            if(isRange(nr, nc) && map[nr][nc] != 2) {
+                bfs(nr, nc);
+            }
+        }
+    }
+
+    private static boolean isRange(int r, int c) {
+        return (0<=r && r<MAX_SIZE && 0<=c && c<MAX_SIZE);
+    }
+
+    private static void printMap() {
+        for(int i=0;i<10;i++) {
+            for(int j=0;j<10;j++) {
+                System.out.print(map[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
