b09b761c6b8657546fefb3ee14b40fb866a1a05d
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-08-15 20:50:12 +0900

    Title: 빙산의 일각, Time: 3032ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

--- 240815/빙산의 일각/the-tip-of-the-iceberg.java
+++ 240815/빙산의 일각/the-tip-of-the-iceberg.java
@@ -17,7 +17,7 @@ public class Main {
             minHeight = Math.min(minHeight, H[i]);
         }
 
-        for(int i=minHeight-1; i<maxHeight; i++) {
+        for(int i=minHeight-1; i<=maxHeight; i++) {
             int cnt = 0;
             boolean check = false;
             for(int j=0;j<N;j++) {
