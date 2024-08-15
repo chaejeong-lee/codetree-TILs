3b5376598389764184004053a500bc877d955c86
Chaejeong <112626357+chaejeong-lee@users.noreply.github.com>
2024-08-15 20:21:22 +0900

    Title: 빙산의 일각, Time: 3094ms, Memory: 18MB, Status: Time Limit Exceed - CodeTree

--- 240815/빙산의 일각/the-tip-of-the-iceberg.java
+++ 240815/빙산의 일각/the-tip-of-the-iceberg.java
@@ -29,7 +29,7 @@ public class Main {
                 }
             }
             
-            if(cnt > result) result = cnt;
+            result = Math.max(result, cnt);
         }
         System.out.println(result);
     }
